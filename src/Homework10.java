c5395b658c8f8406feaa2b4d79909c4dbb8b2ee5
xpycteam88 <sicuno@mail.ru>
2023-10-30 22:00:24 +0300

    Added method join

--- src/Homework10.java
+++ src/Homework10.java
@@ -5,7 +5,9 @@ public class Homework10 {
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
+        String fullNameMethod = String.join(" ", lastName, firstName, middleName);
         System.out.println("ФИО сотрудника - " + fullName);
+        System.out.println("ФИО сотрудника - " + fullNameMethod);
 
         System.out.println("\nЗадача №2");
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
