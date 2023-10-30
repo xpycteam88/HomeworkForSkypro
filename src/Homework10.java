0c36185d72ce5c9893950e20dcc0188169f180f1
xpycteam88 <sicuno@mail.ru>
2023-10-30 21:51:07 +0300

    Added all tasks

--- src/Homework10.java
+++ src/Homework10.java
@@ -1,5 +1,20 @@
 public class Homework10 {
     public static void main(String[] args) {
+        System.out.println("Задача №1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        System.out.println("\nЗадача №2");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("\nЗадача №3");
+        String text = "Данные Ф. И. О. сотрудника — ";
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println(text + fullName);
 
     }
 }
